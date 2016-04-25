7edbba7c30e7b04fc01065b3bd297e37f398738f
unknown <indratmofajar@gmial.com>
2016-04-25 13:41:58 +0700

    testing

---
+++ index.php
@@ -0,0 +1,173 @@
+
+<html>
+<head>
+<title>*** Potpourri Restaurant ***</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<script type="text/javascript">
+<!--
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+//-->
+</script>
+<style>
+body {
+	background-image: url(images/bg.gif);
+	background-repeat: repeat-y;
+	background-position: center;
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+}
+-->
+</style>
+<link href="style.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+<!--
+.style2 {color: #009933}
+-->
+</style>
+</head>
+
+<body bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" onLoad="MM_preloadImages('images/navigasi_08.jpg','images/navigasi_09.jpg','images/navigasi_10.jpg','images/navigasi_11.jpg','images/navigasi_12.jpg')">
+<!-- Save for Web Slices (webfajar.psd) -->
+<table id="Table_01" width="800" height="732" border="0" cellpadding="0" cellspacing="0" align="center" bgcolor="#FFFFFF">
+<tr>
+		<td height="32" colspan="4"><a href="?module=home" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Image17','','images/navigasi_08.jpg',1)"><img src="images/navigasi_02.jpg" name="Image17" width="160" height="32" border="0"></a><a href="?module=product" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Image18','','images/navigasi_09.jpg',1)"><img src="images/navigasi_03.jpg" name="Image18" width="164" height="32" border="0"></a><a href="?module=contact" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Image19','','images/navigasi_10.jpg',1)"><img src="images/navigasi_04.jpg" name="Image19" width="158" height="32" border="0"></a><a href="?module=about" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Image20','','images/navigasi_11.jpg',1)"><img src="images/navigasi_05.jpg" name="Image20" width="161" height="32" border="0"></a><a href="?module=bukutamu" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Image21','','images/navigasi_12.jpg',1)"><img src="images/navigasi_06.jpg" name="Image21" width="157" height="32" border="0"></a></td>
+  </tr>
+	<tr>
+		<td width="167" height="153">
+			<img src="images/web_02.jpg" width="167" height="153" alt=""></td>
+  <td width="1">
+			<img src="images/web_03.jpg" width="1" height="153" alt=""></td>
+		<td width="394">
+			<img src="images/web_04.jpg" width="394" height="153" alt=""></td>
+  <td width="238">
+			<img src="images/web_05.jpg" width="238" height="153" alt=""></td>
+  </tr>
+	<tr>
+		<td height="35" colspan="2" >
+			<img src="images/web_06.jpg" width="168" height="35" alt=""></td>
+	    <td rowspan="4"><table width="100%" border="0" cellspacing="1">
+          <tr>
+            <th colspan="2" scope="col"><marquee>
+              <em>Assalamu'alaikum Wr. Wb. Welcome to the Potpourri Restaurant              </em>
+            </marquee></th>
+          </tr>
+          <tr>
+            <td colspan="2" align="right"><?php print(date("l F d Y"));?></td>
+          </tr>
+          <tr>
+            <td><?php 
+			include "koneksi.php";
+			include "module.php";
+			?></td>
+            <td>&nbsp;</td>
+          </tr>
+        </table></td>
+						
+						
+  <td>
+			<img src="images/web_08.jpg" width="238" height="35" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="2"><table width="100%" border="0" cellspacing="1" class="bgmenu">
+          <tr>
+            <th width="23%" scope="col"><img src="images/star.png" width="37" height="35"></th>
+            <th width="77%" scope="col" align="left"><a href="?module=japan">Japan </a></th>
+          </tr>
+          <tr>
+            <td><img src="images/star.png"></td>
+            <td><a href="?module=thailand"><strong>Thailand</strong></a></td>
+          </tr>
+          <tr>
+            <td><img src="images/star.png"></td>
+            <td><a href="?module=chinese"><strong>Chinese</strong></a></td>
+          </tr>
+          <tr>
+            <td><img src="images/star.png"></td>
+            <td><a href="?module=beverages"><strong>Beverages</strong></a></td>
+          </tr>
+          <tr>
+            <td><img src="images/star.png"></td>
+            <td><a href="?module=promo"><strong>Menu Bulan Ini</strong></a></td>
+          </tr>
+          
+          <tr>
+            <td colspan="2"><div align="center"><img src="images/download (1).jpg" width="155" height="50"></div></td>
+          </tr>
+      </table></td>
+    <td><table width="100%" border="0" cellspacing="1">
+          <tr>
+            <th width="87%" scope="col"><img src="images/slide.gif" width="234" height="205"></th>
+          </tr>
+          <tr>
+            <td><a href="?module=product">detail product>></a></td>
+          </tr>
+        </table></td>
+  </tr>
+	<tr>
+		<td height="35" colspan="2">
+			<img src="images/web_11.jpg" width="168" height="35" alt=""></td>
+  <td>
+			<img src="images/web_12.jpg" width="238" height="35" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="2"><?php 
+		include "koneksi.php";
+		include "sample.php";
+		?></td>
+		<td><table width="98%" height="173" border="0" cellspacing="1" class="bgmenu">
+  <tr>
+    <th height="78" colspan="2" scope="col"><div align="left">Sofyan Hotel Betawi<br>
+      Jl. Cut Meutia No.9<br>
+      Jakarta 10330 - Indonesia<br>
+      http://<a href="http://www.sofyanhotel.com">www.sofyanhotel.com</a></div></th>
+    </tr>
+  <tr>
+    <td colspan="2"><strong>Open at : 10 am - 21 pm</strong></td>
+    </tr>
+  <tr>
+    <td height="32" colspan="2"><blink><span class="style2">For Reservation and Delivery Order Please Contact Us :</span></blink></td>
+    </tr>
+  <tr>
+    <td width="21%" height="32"><strong>Phone</strong></td>
+    <td width="79%"><strong> 0213905011</strong></td>
+  </tr>
+  <tr>
+    <td height="38"><a href="www.yahoo.com"><img src="images/icon-mail.png" width="32" height="32" title="email"></a></td>
+    <td><strong>E-mail</strong></td>
+  </tr>
+</table></td>
+  </tr>
+	<tr>
+		<td>
+			<img src="images/web_15.jpg" width="167" height="32" alt=""></td>
+		<td colspan="2">
+			<img src="images/web_16.jpg" width="395" height="32" alt=""></td>
+		<td>
+			<img src="images/web_17.jpg" width="238" height="32" alt=""></td>
+	</tr>
+</table>
+<!-- End Save for Web Slices -->
+</body>
+</html>
